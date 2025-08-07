2dd24ef66887d7cfac518f7d33b678aa4eb3d482
karungop <karun.gopal@gmail.com>
2025-08-07 12:20:06 -0400

    sure
    sure

--- handtyped-electron/src/preload.js
+++ handtyped-electron/src/preload.js
@@ -3,6 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   saveGesture: (gesture) => ipcRenderer.send('save-gesture', gesture),
   getGestures: () => ipcRenderer.invoke('get-gestures'),
-  pressKey: (key) => ipcRenderer.send('press-key', key)
+  pressKey: (key) => ipcRenderer.send('press-key', key),
+  deleteGesture: (name) => ipcRenderer.send('delete-gesture', name),
 });
 
